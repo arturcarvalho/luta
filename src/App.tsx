d8e37ebc9e320ebce2d9b5f1b559c8bb0b981cf6
Artur Carvalho <arturbelico@gmail.com>
2024-01-06 16:36:21 +0100

    Add basic card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 
+type CardProps = {
+  title: string;  
+};
+
+function Card(props:CardProps) {
+  return (
+    <div className="bg-white text-gray-500 w-96 rounded-md p-4 shadow-md">
+      <h2 className="text-xl ">{props.title}</h2>
+      <p>image here</p>
+    </div>
+  );
+}
 
 function App() {
-  
-
   return (
-    
-      <div className="bg-red-500 p-4 m-4">asdf</div>
-      
-    
+    // main container
+    <div className="bg-pink-500 text-white p-4">
+      <h1 className="text-3xl">Luta!</h1>
+
+      <div className="flex flex-row gap-3">
+        <Card title="Super Mario"/>
+        <Card title="Bowser"/>
+        
+      </div>
+    </div>
   );
 }
 
